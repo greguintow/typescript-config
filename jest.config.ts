2976183c372fe0450123ef7bc669aec1bdf73736
Greg Teixeira <greg.teixeira123@gmail.com>
2021-05-16 16:10:12 -0300

    chore: enable tsconfig paths

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,14 @@
 import type { Config } from '@jest/types'
+import tsPaths from 'tsconfig-paths-jest'
+import tsConfig from './tsconfig.json'
+
+const moduleNameMapper = tsPaths(tsConfig)
 
 export default {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
-	clearMocks: true
+	collectCoverage: true,
+	coverageDirectory: './coverage',
+	clearMocks: true,
+	moduleNameMapper
 } as Config.InitialOptions
